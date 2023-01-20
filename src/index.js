6b01ca090ae6d73e3867ca238a72f32a7716eda9
Brandon Holman <bholmdev@gmail.com>
2023-01-20 17:38:58 -0600

    Inheritance

--- src/index.js
+++ src/index.js
@@ -9,3 +9,16 @@ class Person {
 } ;
 
 const person = new Person("Mosh");
+
+class Teacher extends Person {
+    constructor(name, degree) {
+        super(name);
+        this.degree = degree;
+    }
+
+    teach() {
+        console.log("teach");
+    }
+};
+
+const teacher = new Teacher("Mosh", "MSc");
